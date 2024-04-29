8dcd3a6307b5de2c4263f87be87d03762a42d0e2
mercy-tarus <merciechelah1@gmail.com>
2024-04-29 11:14:05 +0300

    complete

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
+/** @format */
+
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  return (
+    <div>
+      <input
+        type="password"
+        placeholder="entering password"
+        name="password"
+        onChange={() => console.log("Entering password...")}
+      ></input>
+    </div>
+  );
 }
 
 export default Keypad;
